b18cef50631a5f603c87d09e639cad8947551624
福本光起 <fukumoto_mitsuki@fukumotomitsuokinoMacBook-Pro.local>
2022-11-09 22:50:28 +0900

    login done, gmail unfinish

--- observe.py
+++ observe.py
@@ -1,6 +1,8 @@
 import time                            # スリープを使うために必要
 from selenium import webdriver         # Webブラウザを自動操作する（python -m pip install selenium)
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import gmail
 
 ID = "00016732"
 PW = "19971210"
@@ -8,7 +10,16 @@ PW = "19971210"
 def observe():
 	driver = webdriver.Chrome()            # Chromeを準備
 	driver.get('https://user.shinjuku-shisetsu-yoyaku.jp/regasu/reserve/gin_menu')   # ここから始めないと不正な値とか言われるorz
-	time.sleep(5)                          # 5秒間待機
+	time.sleep(5)  # ページ遷移まち
+	driver.find_element(By.XPATH,"/html/body/div/div[2]/ul[1]/li[2]/dl/dt/form/input[2]").click()  # 高機能操作をクリック
+	time.sleep(5)  # ページ遷移待ち
+	driver.find_element(By.XPATH,"/html/body/div/div[2]/div[1]/div/form/div/input[1]").send_keys(ID)
+	time.sleep(1)
+	driver.find_element(By.XPATH,"/html/body/div/div[2]/div[1]/div/form/div/input[2]").send_keys(PW)
+	time.sleep(1)
+	driver.find_element(By.XPATH,"/html/body/div/div[2]/div[1]/div/form/p/input").click()
+	time.sleep(5)
+	gmail.send()
 	driver.quit()
 
 
