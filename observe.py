51d5c0b92ad5f6686fb80ceb2f8647b5ddc9039b
福本光起 <fukumoto_mitsuki@fukumotomitsuokinoMacBook-Pro.local>
2022-11-12 00:09:29 +0900

    ページ遷移後セレニウムの制御外れる・・・

--- observe.py
+++ observe.py
@@ -3,26 +3,63 @@ from selenium import webdriver         # Webブラウザを自動操作する（
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import gmail
+from selenium.webdriver.common.action_chains import ActionChains
 
-ID = "00016732"
-PW = "19971210"
 
-def observe():
-	driver = webdriver.Chrome()            # Chromeを準備
-	driver.get('https://user.shinjuku-shisetsu-yoyaku.jp/regasu/reserve/gin_menu')   # ここから始めないと不正な値とか言われるorz
-	time.sleep(5)  # ページ遷移まち
-	driver.find_element(By.XPATH,"/html/body/div/div[2]/ul[1]/li[2]/dl/dt/form/input[2]").click()  # 高機能操作をクリック
-	time.sleep(5)  # ページ遷移待ち
-	driver.find_element(By.XPATH,"/html/body/div/div[2]/div[1]/div/form/div/input[1]").send_keys(ID)
-	time.sleep(1)
-	driver.find_element(By.XPATH,"/html/body/div/div[2]/div[1]/div/form/div/input[2]").send_keys(PW)
-	time.sleep(1)
-	driver.find_element(By.XPATH,"/html/body/div/div[2]/div[1]/div/form/p/input").click()
-	time.sleep(5)
-	gmail.send()
-	driver.quit()
+
+class Observer:
+	ID = "00016732"
+	PW = "19971210"
+	driver = webdriver.Chrome()
+	def sclick(self,xpath,shift=False):
+		time.sleep(1)
+		if(shift):
+			element = self.driver.find_element(By.XPATH,xpath)
+			actions = ActionChains(self.driver)  # ActionChainを作成
+			actions.key_down(Keys.SHIFT)
+			actions.click(element)
+			actions.perform()
+		else:
+			self.driver.find_element(By.XPATH,xpath).click()
+	def sclicks(self,xpaths,shift = False):
+		for xpath in xpaths:
+			self.sclick(xpath,shift)
+
+	def observe(self):
+		self.driver.get('https://user.shinjuku-shisetsu-yoyaku.jp/regasu/reserve/gin_menu')   # ここから始めないと不正な値とか言われるorz
+		self.sclick("/html/body/div/div[2]/ul[1]/li[2]/dl/dt/form/input[2]")  # 高機能操作をクリック
+		time.sleep(1)
+		self.driver.find_element(By.XPATH,"/html/body/div/div[2]/div[1]/div/form/div/input[1]").send_keys(self.ID)
+		time.sleep(1)
+		self.driver.find_element(By.XPATH,"/html/body/div/div[2]/div[1]/div/form/div/input[2]").send_keys(self.PW)
+		self.sclicks([
+		"/html/body/div/div[2]/div[1]/div/form/p/input",
+		"/html/body/div/div[2]/div[1]/dl/dd/ul/li[1]/a",
+		"/html/body/div/div[2]/div[2]/form[1]/div/div/div/div[1]/div/div[1]/select[1]/option[2]",
+		"/html/body/div/div[2]/div[2]/form[1]/div/div/div/div[1]/div/div[1]/input[2]",
+		"/html/body/div/div[2]/div[2]/form[4]/div/div/div/div[1]/div/div[1]/select",
+		"/html/body/div/div[2]/div[2]/form[4]/div/div/div/div[1]/div/div[1]/select/option[2]",
+		"/html/body/div/div[2]/div[2]/form[4]/div/div/div/div[1]/div/div[1]/input[2]"])
+		# Ctr を押しながらクリックする
+		self.sclicks([
+		"/html/body/div/div[2]/div[2]/div[2]/form/div/div/div[1]/div/div[1]/select/option[3]",
+		"/html/body/div/div[2]/div[2]/div[2]/form/div/div/div[1]/div/div[1]/select/option[4]"],shift=True)
+		self.sclick("/html/body/div/div[2]/div[2]/div[2]/form/div/div/div[1]/div/div[1]/input[2]")
+		self.sclicks([
+		"/html/body/div/div[2]/div[2]/form[5]/div/div/div/select/option[1]",
+		"/html/body/div/div[2]/div[2]/form[5]/div/div/div/select/option[2]"],shift=True)
+		self.sclicks([
+		"/html/body/div/div[2]/div[2]/form[5]/div/div/div/p[2]/input[2]",
+		"/html/body/div/div[2]/div[2]/form[6]/p/input"])
+		time.sleep(3)  # ページ遷移を挟むため少し待つ
+		self.sclick("/html/body/div/div[2]/div[2]/div[2]/div/div/ul[1]/li/a/img")
+
+		time.sleep(5)
+		#gmail.send()
+		self.driver.quit()
 
 
 
 if __name__ == '__main__':
-	observe()
+	observer = Observer()
+	observer.observe()
