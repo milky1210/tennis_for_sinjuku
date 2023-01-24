7766a8840368ad377d841533d8298cab4404e633
福本光起 <fukumoto_mitsuki@fukumotomitsuokinoMacBook-Pro.local>
2023-01-24 20:22:54 +0900

    helpを表示

--- main.py
+++ main.py
@@ -50,13 +50,13 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--month", default=0, type=int)
-    parser.add_argument("--input", default="")
-    parser.add_argument("--addSun", action="store_true")
-    parser.add_argument("--addSat", action="store_true")
-    parser.add_argument("--addNight", action="store_true")
-    parser.add_argument("--repeat", default=60, type=int)  # min
-    parser.add_argument("--addDays", default=[], nargs="*", type=int)  # 日付のリスト
-    parser.add_argument("--hide", action="store_true")
+    parser.add_argument("--month", default=0, type=int,help="今月か来月かを0,1で指定.0:今月、1:来月")
+    parser.add_argument("--input", default="",help="日時と時間のarrayファイルを指定.形式はcsv(未実装)")
+    parser.add_argument("--addSun", action="store_true", help = "日曜日を追加,時間は9-19まで")
+    parser.add_argument("--addSat", action="store_true",help = "土曜日を追加,時間は9-19まで")
+    parser.add_argument("--addNight", action="store_true", help="夜(19-)の枠のみを表示")
+    parser.add_argument("--repeat", default=60, type=int, help="繰り返しのインターバル[min](未実装)")  # min
+    parser.add_argument("--addDays", default=[], nargs="*", type=int,help="追加したい日付スペース区切りで羅列.(ex: 10 31)")  # 日付のリスト
+    parser.add_argument("--hide", action="store_true",help = "ドライバを非表示に設定")
     args = parser.parse_args()
     main(args)
